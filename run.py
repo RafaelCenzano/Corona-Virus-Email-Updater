f220950a0ab050bd4a39ac2697dd231bca88463d
Savagecoder77 <savagecoder77@gmail.com>
2020-03-09 10:52:03 -0700

    include percentage changes

--- run.py
+++ run.py
@@ -8,6 +8,11 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 
+def findValue(str):
+    temp = [int(s) for s in str.split() if s.isdigit()]
+    return temp[0]
+
+
 def scraper():
     r = requests.get('https://www.cdc.gov/coronavirus/2019-ncov/cases-in-us.html')
     page = r.text
@@ -15,24 +20,58 @@ def scraper():
 
 
     f = open('cdc-date.txt', 'r')
-    date = f.read()
+    date = f.readlines()
     f.close()
 
     dates = soup.find_all(attrs={'class':'text-red'})
     pageDate = dates[0].text
 
-    if pageDate != date:
+    if pageDate != date[0]:
 
         fnew = open('cdc-date.txt', 'w')
         fnew.write(pageDate)
+
+        div = soup.find(attrs={'class':'card-body bg-white'})
+
+        litags = div.find_all('li')
+        cases = litags[0].text
+        deaths = litags[1].text
+        statesWith = litags[2].text
+
+        fnew.write(f'\n{cases}')
+        fnew.write(f'\n{deaths}')
+        fnew.write(f'\n{statesWith}')
         fnew.close()
 
-        litags = soup.find_all('li')
-        cases = litags[11].text
-        deaths = litags[12].text
-        statesWith = litags[13].text
+        nowFormatted = datetime.now().strftime('%-m/%-d/%y %-I:%M %p')
 
-        nowFormatted = datetime.now().strftime('%-m/%-d/%y %-I:%-M %p')
+        currentCases = findValue(cases)
+        currentDeaths = findValue(deaths)
+        currentStates = findValue(statesWith)
+        pastCases = findValue(date[1])
+        pastDeaths = findValue(date[2])
+        pastStates = findValue(date[3])
+
+        if currentCases > pastCases:
+            casesPercentage = ((currentCases - pastCases) / pastCases) * 100
+            changeCases = 'increase'
+        else:
+            casesPercentage = ((pastCases - currentCases) / pastCases) * 100
+            changeCases = 'decrease'
+
+        if currentDeaths > pastDeaths:
+            deathsPercentage = ((currentDeaths - pastDeaths) / pastDeaths) * 100
+            changeDeaths = 'increase'
+        else:
+            deathsPercentage = ((pastDeaths - currentDeaths) / pastDeaths) * 100
+            changeDeaths = 'decrease'
+
+        if currentStates > pastStates:
+            statesPercentage = ((currentStates - pastStates) / pastStates) * 100
+            changeStates = 'increase'
+        else:
+            statesPercentage = ((pastStates - currentStates) / pastStates) * 100
+            changeStates = 'decrease'
 
         email_message = (f'''
 Hello,
@@ -42,14 +81,19 @@ Update: {nowFormatted}
 CDC updated @ {pageDate}
 
 {cases}
-{deaths}
-{statesWith}
+{casesPercentage}% {changeCases} in cases in U.S.
 
-- Covid-19 reporter
+{deaths}
+{deathsPercentage}% {changeDeaths} in deaths in U.S.
+
+{statesWith}
+{statesPercentage}% {changeStates} in states with cases in U.S.
+
+- COVID-19 Reporter
                           ''')
 
         msg = MIMEMultipart()
-        msg['From'] = f'covid-19 updater <{senderEmail}>'
+        msg['From'] = f'COVID-19 Reporter <{senderEmail}>'
         msg['To'] = recieverEmail
         msg['Subject'] = f'Update: {nowFormatted}'
         msg.attach(MIMEText(email_message,'plain'))
